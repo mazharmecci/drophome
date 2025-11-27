b38fca205c40f6b60624bbbba8c70b603f2b7200
openvault <157576678+mazharmecci@users.noreply.github.com>
2025-11-27 14:44:50 +0530

    Update firebase.js

--- scripts/firebase.js
+++ scripts/firebase.js
@@ -1,1 +1,14 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqFJ85euyPb4QV863AmBF9zHv34WIdmrg",
+  authDomain: "drophome-1cb76.firebaseapp.com",
+  projectId: "drophome-1cb76",
+  storageBucket: "drophome-1cb76.firebasestorage.app",
+  messagingSenderId: "268666785164",
+  appId: "1:268666785164:web:6b02f3242b9e21fad25aa9"
+};
+
+export const app = initializeApp(firebaseConfig);
 
